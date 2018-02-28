fca2673001be1a1889ace88f6f0dd36f5d19407b
Katleiah <katleiahramos@gmail.com>
2018-02-28 21:58:18 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
 def valid_move?(board,index)
- if position_taken?
+ if position_taken?(board,index)
    return false
  # elsif index > 9 || index < 0
 #   return false
